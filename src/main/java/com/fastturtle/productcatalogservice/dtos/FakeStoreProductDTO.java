642a7f72e0e900deb98e5cbc80cbeb2cbe8bcdfd
Divya Gupta <divygupta0319@gmail.com>
2024-10-31 16:30:58 +0530

    Added FakeStore api related product DTO in our code

---
+++ src/main/java/com/fastturtle/productcatalogservice/dtos/FakeStoreProductDTO.java
@@ -0,0 +1,23 @@
+package com.fastturtle.productcatalogservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreProductDTO {
+
+    private Long id;
+
+    private String title;
+
+    private Double price;
+
+    private String description;
+
+    private String category;
+
+    private String image;
+
+    private FakeStoreRatingDTO rating;
+}
---
+++ src/main/java/com/fastturtle/productcatalogservice/dtos/FakeStoreRatingDTO.java
@@ -0,0 +1,13 @@
+package com.fastturtle.productcatalogservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreRatingDTO {
+
+    private Double rate;
+
+    private Long count;
+}
