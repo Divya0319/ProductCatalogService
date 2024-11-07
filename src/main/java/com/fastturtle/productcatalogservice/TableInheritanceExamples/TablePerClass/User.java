719945dea6105296cbc0471b6cb363594d0ada11
Divya Gupta <divygupta0319@gmail.com>
2024-11-07 17:14:23 +0530

    Added Table per class inheritance type in User

--- src/main/java/com/fastturtle/productcatalogservice/TableInheritanceExamples/TablePerClass/User.java
+++ src/main/java/com/fastturtle/productcatalogservice/TableInheritanceExamples/TablePerClass/User.java
@@ -2,8 +2,11 @@ package com.fastturtle.productcatalogservice.TableInheritanceExamples.TablePerCl
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity(name = "tpc_user")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class User {
 
     @Id
