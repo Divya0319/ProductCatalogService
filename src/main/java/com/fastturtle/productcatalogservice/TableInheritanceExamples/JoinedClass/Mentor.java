7c91ae4e938d22bfb8118a5b26182ea87611f155
Divya Gupta <divygupta0319@gmail.com>
2024-11-07 19:15:10 +0530

    Added Joined class inheritance classes

---
+++ src/main/java/com/fastturtle/productcatalogservice/TableInheritanceExamples/JoinedClass/Mentor.java
@@ -0,0 +1,11 @@
+package com.fastturtle.productcatalogservice.TableInheritanceExamples.JoinedClass;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
+@Entity(name = "jc_mentor")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Mentor extends User {
+
+    private Long hours;
+}
