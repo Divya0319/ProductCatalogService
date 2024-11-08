6e3516b6595018c1f3be2cf0e3f02302237389a9
divya0319 <divygupta0319@gmail.com>
2024-11-08 20:23:01 +0530

    Added DB schema migration files using JPA buddy

---
+++ src/main/java/com/fastturtle/productcatalogservice/models/TestModel.java
@@ -0,0 +1,10 @@
+package com.fastturtle.productcatalogservice.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class TestModel extends BaseModel {
+
+    private Integer numField;
+
+}
