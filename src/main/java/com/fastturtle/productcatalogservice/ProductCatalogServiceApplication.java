c4e04726b4cfe39b7815b860f69755f955096670
divya0319 <divygupta0319@gmail.com>
2024-11-16 18:10:47 +0530

    Added todo comment to re-enable jpa auditing once testing done

--- src/main/java/com/fastturtle/productcatalogservice/ProductCatalogServiceApplication.java
+++ src/main/java/com/fastturtle/productcatalogservice/ProductCatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //@EnableJpaAuditing
+//TODO uncomment when testing done
 public class ProductCatalogServiceApplication {
 
     public static void main(String[] args) {
