c86b5021c66799d313f07bf97666550b31114dca
divya0319 <divygupta0319@gmail.com>
2024-11-17 12:43:39 +0530

    Created a POST api mvc test for createProduct_RunsSuccessfully scenario

--- src/test/java/com/fastturtle/productcatalogservice/controllers/ProductControllerMvcTests.java
+++ src/test/java/com/fastturtle/productcatalogservice/controllers/ProductControllerMvcTests.java
@@ -1,19 +1,23 @@
 package com.fastturtle.productcatalogservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fastturtle.productcatalogservice.dtos.ProductDTO;
 import com.fastturtle.productcatalogservice.models.Product;
 import com.fastturtle.productcatalogservice.service.IProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,4 +64,24 @@ public class ProductControllerMvcTests {
     }
 
     // TODO : try mockmvc test for post api
+
+    @Test
+    public void test_CreateProduct_RunsSuccessfully() throws Exception {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(5L);
+        productDTO.setName("ipad");
+        productDTO.setDescription("writing ipad");
+
+        Product product = new Product();
+        product.setId(5L);
+        product.setName("ipad");
+        product.setDescription("writing ipad");
+
+        when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+        mockMvc.perform(post("/products").content(objectMapper.writeValueAsString(productDTO)).contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(productDTO)));
+    }
 }
